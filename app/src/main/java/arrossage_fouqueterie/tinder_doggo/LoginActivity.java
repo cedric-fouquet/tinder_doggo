9aef68b4e6c7de9b31db64e6945791e40ff81b45
cedric <cedric.fouquet@etu.univ-amu.fr>
2018-05-16 17:07:16 +0200

    Avancé systeme de login

--- app/src/main/java/arrossage_fouqueterie/tinder_doggo/LoginActivity.java
+++ app/src/main/java/arrossage_fouqueterie/tinder_doggo/LoginActivity.java
@@ -33,11 +33,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.lang.annotation.Inherited;
@@ -57,7 +60,7 @@ public class LoginActivity extends BaseActivity  {
     private static final String TAG = "Login";
     private static final int REQUEST_READ_CONTACTS = 0;
     private FirebaseAuth mAuth;
-
+    private Exception testEx;
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
@@ -115,58 +118,77 @@ public class LoginActivity extends BaseActivity  {
         }
     }
     private void createAccount(String email, String password)throws FirebaseAuthException{
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            try{
-                                throw  task.getException();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-
-                    }
-                });
+        mAuth.createUserWithEmailAndPassword(email, password).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }).addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
+            }
+        });
     }
-    private void signIn(String email, String password) throws FirebaseAuthException{
+    private void signIn(final String email, final String password) {
         mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUI(user);
-                        } else {
-                            try{
-                                throw  task.getException();
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInWithEmail:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+//                        } else {
+//                            try{
+//                                throw  task.getException();
+//                            } catch (Exception e) {
+//                                e.printStackTrace();
+//                            }
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+//
+//                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                            updateUI(null);
+//                        }
+//
+//
+//                    }
+//                })
+ .addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Log.d(TAG, "signInWithEmail:success");
+                FirebaseUser user = mAuth.getCurrentUser();
+                updateUI(user);
 
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUI(null);
-                        }
-
-
+            }
+        })
+.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if(e instanceof FirebaseAuthInvalidUserException)
+                {
+                    Log.d(TAG, "signInWithEmail:FirebaseAuthInvalidUserException");
+                    Log.d(TAG, e.getMessage());
+                    Log.d(TAG, e.getLocalizedMessage());
+                    try {
+                        createAccount(email,password);
+                    } catch (FirebaseAuthException e1) {
+                        e1.printStackTrace();
                     }
-                });
+                }
+                else{
+                    Log.d(TAG, "signInWithEmail:failure");
+                }
+
+            }
+        });
 
     }
 
@@ -284,30 +306,12 @@ public class LoginActivity extends BaseActivity  {
         UserLoginTask(String email, String password) {
             mEmail = email;
             mPassword = password;
+
         }
 
         @Override
         protected Boolean doInBackground(Void... params) {
-
-            try {
-                signIn(mEmail,mPassword);
-
-            } catch(FirebaseAuthException e){
-                Log.d(TAG, "loginToFireBase:failure");
-                try{
-                    createAccount(mEmail,mPassword);
-                }
-                catch(FirebaseAuthException registerE){
-
-                    Log.d(TAG, "registerToFireBase:failure");
-                    return false;
-                }
-            }
-            catch(Exception e){
-                Log.d(TAG, "unkownLoginError:failure");
-                return false;
-            }
-
+            signIn(mEmail,mPassword);
 
 
             return true;
