7f70eea8b6adfb133afb0caf2cae662e55e12554
cedric <cedric.fouquet@etu.univ-amu.fr>
2018-06-13 12:47:29 +0200

    matcher avancé

--- app/src/main/java/arrossage_fouqueterie/tinder_doggo/DoggoMatcher.java
+++ app/src/main/java/arrossage_fouqueterie/tinder_doggo/DoggoMatcher.java
@@ -24,9 +24,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -35,7 +38,8 @@ public class DoggoMatcher extends BaseActivity {
     private FusedLocationProviderClient mFusedLocationClient;
     private static final String TAG = "DoggoMatcher";
 
-    private FirebaseDatabase mDatabase;
+    private StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+    private FirebaseDatabase database;
     private FirebaseAuth mAuth;
 
     private TextView nameView;
@@ -44,13 +48,13 @@ public class DoggoMatcher extends BaseActivity {
     private ImageButton acceptDoggoButton;
     private ImageButton rejectDoggoButton;
     private ImageView profilePicture;
-    private List<DoggoProfile> usersList;
+    private List<DoggoProfile> usersList =  new ArrayList<DoggoProfile>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance();
+        database = FirebaseDatabase.getInstance();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doggo_matcher);
@@ -70,61 +74,102 @@ public class DoggoMatcher extends BaseActivity {
         profilePicture.setVisibility(View.INVISIBLE);
 
         final TextView nameView = findViewById(R.id.textViewName);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
 
-            return;
-        }
-        Log.d(TAG, mFusedLocationClient.toString());
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-
-                            mDatabase.getReference().child("positions").child(mAuth.getUid()).setValue(location);
-                            Log.d(TAG, location.toString());
-                        }
-                    }
-
-                }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d(TAG, "onfailure: ");
-            }
-        });
-
-        DatabaseReference myRef = mDatabase.getReference().child("user");
-
+        DatabaseReference myRef = database.getReference().child("user");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot snapshot : dataSnapshot.getChildren())
-                {
-                    usersList.add(snapshot.getValue(DoggoProfile.class));
+                Log.d(TAG, "onDataChange: "+dataSnapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+
+                    String currentId = dataSnapshot.child("profileId").getValue(String.class);
+                    if( currentId == mAuth.getUid()){
+                        break;
+                    }
+
+                    final DoggoProfile doggerino = new  DoggoProfile( dataSnapshot.child("profileId").getValue(String.class));
+                    doggerino.loadProfileInfo(postSnapshot);
+
+                    //getimage
+                    StorageReference image = storageRef.child(mAuth.getUid()+".jpg");
+
+                    final long ONE_MEGABYTE = 1024 * 1024 * 100;
+
+                    image.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                        @Override
+                        public void onSuccess(byte[] bytes) {
+                            doggerino.loadImage(bytes);
+                            usersList.add(doggerino);
+                            if(!usersList.isEmpty())
+                            {
+                                showCurrentUser(0);
+                            }
+
+                        }
+                    });
+
                 }
+
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                // Getting Post failed, log a message
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                // ...
             }
         });
+        //TODO : Implémenter la position si t'as le temps :'(
+//
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//
+//            return;
+//        }
+//        Log.d(TAG, mFusedLocationClient.toString());
+//        mFusedLocationClient.getLastLocation()
+//                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//                    @Override
+//                    public void onSuccess(Location location) {
+//                        if (location != null) {
+//
+//                            mDatabase.getReference().child("positions").child(mAuth.getUid()).setValue(location);
+//                            Log.d(TAG, location.toString());
+//                        }
+//                    }
+//
+//                }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Log.d(TAG, "onfailure: ");
+//            }
+//        });
+
+
+
+
     }
     protected void showCurrentUser(int listIndex){
 
+        nameView.setText(usersList.get(listIndex).getUsername());
         nameView.setVisibility(View.VISIBLE);
+
+        ageView.setText(Integer.toString(usersList.get(listIndex).getage()));
         ageView.setVisibility(View.VISIBLE);
+        raceView.setText(usersList.get(listIndex).getrace());
         raceView.setVisibility(View.VISIBLE);
+
         acceptDoggoButton.setVisibility(View.VISIBLE);
         rejectDoggoButton.setVisibility(View.VISIBLE);
+        profilePicture.setImageBitmap(usersList.get(listIndex).getProfilePicture());
         profilePicture.setVisibility(View.VISIBLE);
+
     }
 
 
